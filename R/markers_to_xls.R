588d2291fbb3899cb40c2e801529c192fe2d8c10
GrigoriiNos <nosgrigoriy@gmail.com>
2018-11-26 10:32:09 +0100

    ok2

---
+++ R/markers_to_xls.R
@@ -0,0 +1,26 @@
+#' getting your markers into the excel shpreadsheet
+#'
+#' This function allows you write Seurat::FindAllMarkers function output into the excel file having each cluster markers written in the distinct sheet
+#' @param markers_table The output of the Seurat::FndAllMarkers function, don't change anything in the column names within it.
+#' @param filename
+#' @keywords excel, single cell, RNA-seq, Seurat, markers
+#' @import dplyr
+#' @export
+#' @examples
+#' annotate_markers(my_markers, "stromal_markers")
+
+markers_to_xls <- function(markers_table, filename = 'my_markers'){
+  clusters <- 0:(length(
+    unique(markers_table$cluster))-1
+  )
+  tables <- list()
+  for (i in 1:length(clusters)){
+    sub_markers_table <- markers_table %>%
+      filter(cluster == clusters[i])
+    tables[[i]] <- sub_markers_table
+  }
+  WriteXLS::WriteXLS(x = tables,
+                     ExcelFileName = paste0(filename, '.xls'),
+                     SheetNames = paste(clusters, 'cluster')
+  )
+}
