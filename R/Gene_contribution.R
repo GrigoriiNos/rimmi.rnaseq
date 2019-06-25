8743b486960d9132cfb0d6432cf0a8779e0153a8
GrigoriiNos <nosgrigoriy@gmail.com>
2019-06-25 12:29:14 +0200

    solved redundancy and added divisors for more simmetric hm

--- R/Gene_contribution.R
+++ R/Gene_contribution.R
@@ -18,53 +18,69 @@
 #'
 
 
-Gene_contribution <- function(Seurat_obj, ident, signature){
+Gene_contribution <- function(Seurat_obj, ident, signature, signature_name){
   library(gplots)
   library(Seurat)
-  
-  print('you store the signature after calculation in the 1st row of the matrix, it should have a name that you gave it')
-  print('so make sure that 1st row of the matrix is signature')
-  print(rownames(Seurat_obj@data)[1])
-  print('if that was a gene name, then it was wrong, you didnt calculate the avarage expression')
-  
-  Seurat_obj <- SubsetData(Seurat_obj, 
+
+  divisors <- function(x){
+    #  Vector of numberes to test against
+    y <- seq_len(x)
+    #  Modulo division. If remainder is 0 that number is a divisor of x so return it
+    y[ x%%y == 0 ]
+  }
+
+
+  # make an object subset with the given cluster
+  Seurat_obj <- SubsetData(Seurat_obj,
                        ident.use = as.character(ident)
   )
-  
+  signature <- unique(
+    intersect(signature, rownames(Seurat_obj@data))
+    )
+
+  sign_index <- which(rownames(Seurat_obj@data) == signature_name)
+
   genes_percentage <- c()
+  # take a percentage for each gene into a vector
   for (i in 1:length(signature)){
-    
+
     gene_index <- which(rownames(Seurat_obj@data) == signature[i])
-    
-    gene_contrib <- Seurat_obj@data[gene_index,] / Seurat_obj@data[1,]
-    
+
+    gene_contrib <- Seurat_obj@data[gene_index,] / Seurat_obj@data[sign_index,]
+
     q <- sum(gene_contrib) / length(gene_contrib)
-    
+
     genes_percentage <- c(genes_percentage, q)
-    
+
   }
   names(genes_percentage) <- signature
+  # scale it
   genes_percentage <- scale(
-    sort(genes_percentage, 
+    sort(genes_percentage,
          decreasing = T)
   )
-  
+
   df <- data.frame(
     genes = rownames(genes_percentage),
     data = unname(genes_percentage[,1])
   )
-  
-  heatmap.2(matrix(df$data, ncol = 3), 
-            trace="n", 
-            Colv = FALSE, 
+
+  # get a divisor for the length of signature so the heatmap is simmetrical
+  q <- divisors(length(signature))
+  q <- q[q > 2][1]
+
+  # plot it
+  heatmap.2(matrix(df$data, ncol = q),
+            trace="n",
+            Colv = FALSE,
             Rowv=FALSE,
-            cellnote = matrix(df$genes, ncol = 3), 
-            notecol = 1, 
+            cellnote = matrix(df$genes, ncol = q),
+            notecol = 1,
             scale = "none",
-            dendrogram = "none", 
-            labCol = "", 
-            labRow = "", 
+            dendrogram = "none",
+            labCol = "",
+            labRow = "",
             cexRow = 0.75)
-  
+
   print(genes_percentage)
 }
