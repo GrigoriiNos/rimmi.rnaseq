46574cd02bccdbf987998bfa3bd01cc11550c01c
GrigoriiNos <nosgrigoriy@gmail.com>
2019-06-27 15:24:28 +0200

    updated gene contributon

--- R/Gene_contribution.R
+++ R/Gene_contribution.R
@@ -3,8 +3,9 @@
 #' This function allows you to see how did each gene from the given transcriptional program contribute to the pre-calculated gene signature
 #'
 #' @param Seurat_obj your Seurat object
-#' @param ident number of cluster you want to look at
+#' @param resolution what resolution you wanna check, either 0.6 or 1 or 2
 #' @param signature the gene set of your interest
+#' @param signature_name the name you gave to the signature
 #'
 #' @return heatmap with the gene contribution
 #'
@@ -18,41 +19,58 @@
 #'
 
 
-Gene_contribution <- function(Seurat_obj, ident, signature, signature_name){
+Gene_contribution <- function(Seurat_obj,
+                              resolution,
+                              signature,
+                              signature_name{
   library(gplots)
   library(Seurat)
 
-  divisors <- function(x){
-    #  Vector of numberes to test against
-    y <- seq_len(x)
-    #  Modulo division. If remainder is 0 that number is a divisor of x so return it
-    y[ x%%y == 0 ]
+  sign_index <- which(rownames(Seurat_obj@data) == signature_name)
+
+  if (resolution == 0.6){
+    ident <- sort(
+      unique(
+        Seurat_obj@meta.data$res.0.6))
+  }
+  if (resolution == 1){
+    ident <- sort(
+      unique(
+        Seurat_obj@meta.data$res.1))
+  }
+  if (resolution == 2){
+    ident <- sort(
+      unique(
+        Seurat_obj@meta.data$res.2))
+  } else{
+    print('nonono pick either 06 or 1 or 2 resolution')
   }
 
-
+  output <- data.frame(genes = signature)
+  for (i in 1:length(ident)){
   # make an object subset with the given cluster
-  Seurat_obj <- SubsetData(Seurat_obj,
-                       ident.use = as.character(ident)
+  sub_Seurat_obj <- SubsetData(Seurat_obj,
+                       ident.use = ident[i]
   )
-  signature <- unique(
-    intersect(signature, rownames(Seurat_obj@data))
-    )
-
-  sign_index <- which(rownames(Seurat_obj@data) == signature_name)
 
   genes_percentage <- c()
   # take a percentage for each gene into a vector
-  for (i in 1:length(signature)){
+    for (j in 1:length(signature)){
 
-    gene_index <- which(rownames(Seurat_obj@data) == signature[i])
+      gene_index <- which(rownames(sub_Seurat_obj@data) == signature[j])
 
-    gene_contrib <- Seurat_obj@data[gene_index,] / Seurat_obj@data[sign_index,]
+      scaler <- max(
+        abs(
+          sub_Seurat_obj@data[sign_index,])
+        )
 
-    q <- sum(gene_contrib) / length(gene_contrib)
+      gene_contrib <- sub_Seurat_obj@data[gene_index,] + scaler / sub_Seurat_obj@data[sign_index,] + scaler
 
-    genes_percentage <- c(genes_percentage, q)
+      q <- sum(gene_contrib) / length(gene_contrib)
 
-  }
+      genes_percentage <- c(genes_percentage, q)
+    }
+
   names(genes_percentage) <- signature
   # scale it
   genes_percentage <- scale(
@@ -64,23 +82,17 @@ Gene_contribution <- function(Seurat_obj, ident, signature, signature_name){
     genes = rownames(genes_percentage),
     data = unname(genes_percentage[,1])
   )
+  colnames(df)[2] <- paste('cluster', ident[i])
+  output <- merge(output, df, 'genes')
+  }
+  write.csv(output,
+            quote = F,
+            file = paste0('genes from the ',signature_name, ' signature, resolution', resolution, '.csv')
+            )
 
-  # get a divisor for the length of signature so the heatmap is simmetrical
-  q <- divisors(length(signature))
-  q <- q[q > 2][1]
+  rownames(output) <- output$genes
 
-  # plot it
-  heatmap.2(matrix(df$data, ncol = q),
-            trace="n",
-            Colv = FALSE,
-            Rowv=FALSE,
-            cellnote = matrix(df$genes, ncol = q),
-            notecol = 1,
-            scale = "none",
-            dendrogram = "none",
-            labCol = "",
-            labRow = "",
-            cexRow = 0.75)
-
-  print(genes_percentage)
+  heatmap(
+    as.matrix(output[,-1])
+    )
 }
