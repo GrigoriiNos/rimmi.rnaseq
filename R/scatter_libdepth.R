a463cc15909461e499bfc660bf5d2141642324a0
GrigoriiNos <nosgrigoriy@gmail.com>
2019-07-11 15:45:54 +0200

    new function scatter libdepth

---
+++ R/scatter_libdepth.R
@@ -0,0 +1,28 @@
+#' Plot clusters in 2 umaps with the point size corresponting to the library size
+#'
+#' This function allows you to see if the library size influences in your clustering
+#'
+#' @param Seurat_obj your Seurat object
+#'
+#' @return scatter plot with the library size correponding to the point size
+#'
+#' @keywords Seurat, single cell sequencing, RNA-seq, gene signature
+#'
+#' @examples
+#'
+#' scatter_libdepth(A07)
+#'
+#' @export
+#'
+
+scatter_libdepth <- function(Seurat_obj){
+  df <- data.frame(umap1 = Seurat_obj@dr$umap@cell.embeddings[,1 ],
+                   umap2 = Seurat_obj@dr$umap@cell.embeddings[,2 ],
+                   cells = as.character(Seurat_obj@ident),
+                   depth = Seurat_obj@meta.data$nUMI
+                   )
+
+  ggplot(df, aes(x = umap1, y = umap2, col = cells))+
+    geom_point(aes(size = depth)) +
+    scale_size_continuous(range = c(0.001, 3))
+}
