4b0cc3eb35095c536f51e71bb07b60c1ec78c637
GrigoriiNos <nosgrigoriy@gmail.com>
2019-07-15 10:30:39 +0200

    added ngene annotation

--- R/scatter_libdepth.R
+++ R/scatter_libdepth.R
@@ -19,10 +19,15 @@ scatter_libdepth <- function(Seurat_obj){
   df <- data.frame(umap1 = Seurat_obj@dr$umap@cell.embeddings[,1 ],
                    umap2 = Seurat_obj@dr$umap@cell.embeddings[,2 ],
                    cells = as.character(Seurat_obj@ident),
-                   depth = Seurat_obj@meta.data$nUMI
+                   lsize = Seurat_obj@meta.data$nUMI,
+                   ngene = Seurat_obj@meta.data$nGene
                    )
 
-  ggplot(df, aes(x = umap1, y = umap2, col = cells))+
-    geom_point(aes(size = depth)) +
-    scale_size_continuous(range = c(0.001, 3))
+  p1 <- ggplot(df, aes(x = umap1, y = umap2, col = cells))+
+          geom_point(aes(size = lsize)) +
+          scale_size_continuous(range = c(0.001, 3))
+  p2 <- ggplot(df, aes(x = umap1, y = umap2, col = cells))+
+          geom_point(aes(size = ngene)) +
+          scale_size_continuous(range = c(0.001, 3))
+  plot_grid(p1, p2)
 }
