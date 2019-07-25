ca55f2bdf3edee9805ecebf9dde8b4136032acdf
GrigoriiNos <nosgrigoriy@gmail.com>
2019-07-25 13:40:22 +0200

    added cell phone db for seurat

--- R/cellphone.R
+++ R/cellphone.R
@@ -18,22 +18,54 @@
 
 cellphone_for_seurat <- function(Seurat_obj){
 
-  counts <- as.matrix(
-    Seurat_obj@data)
+  counts <- as.data.frame(
+    as.matrix(
+      Seurat_obj@data)
+    )
+
+  #colnames(counts) <- paste('d-pos_', colnames(counts), sep = '')
+
+  library("biomaRt")
+
+  ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
+  genes  <-  getBM(filters='hgnc_symbol',
+            attributes = c('ensembl_gene_id','hgnc_symbol'),
+            values = rownames(counts),
+            mart = ensembl)
+
+  counts <- counts[rownames(counts) %in% genes$hgnc_symbol,]
+
+  counts <- tibble::rownames_to_column(
+    as.data.frame(counts), var = 'hgnc_symbol')
+
+  counts <- plyr::join(counts, genes)
+
+  counts$hgnc_symbol <- NULL
+
+  counts <- cbind(counts[,which(colnames(counts) == 'ensembl_gene_id')], counts)
+
+  colnames(counts)[1] <- 'Gene'
+  counts$ensembl_gene_id <- NULL
 
   metadata <- data.frame(Cell = rownames(Seurat_obj@meta.data),
-                         cell_type = Seurat_obj@meta.data$res.0.6
+                         cell_type = Seurat_obj@ident
                          )
 
-  write.table(couts,
+  #metadata$Cell <- paste('d-pos_', metadata$Cell, sep = '')
+
+  write.table(counts,
             file = 'counts.txt',
             quote = F,
-            col.names = T)
+            col.names = T,
+            row.names = F,
+            sep = '\t')
 
   write.table(metadata,
             file = 'metadata.txt',
             quote = F,
-            col.names = T)
+            col.names = T,
+            row.names = F,
+            sep = '\t')
 
   system('cellphonedb method statistical_analysis metadata.txt counts.txt --iterations=10 --threads=2')
 
@@ -41,3 +73,15 @@ cellphone_for_seurat <- function(Seurat_obj){
 
   system('cellphonedb plot heatmap_plot metadata.txt')
 }
+
+
+
+
+
+
+
+
+
+
+
+
