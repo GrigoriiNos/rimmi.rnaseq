8fe1ea32bdc1d60387dca8673079c0ca18731bbf
GrigoriiNos <nosgrigoriy@gmail.com>
2019-12-10 14:20:39 +0100

    added pull isotype function

--- R/Isotypes_DEG.R
+++ R/Isotypes_DEG.R
@@ -1,8 +1,6 @@
 #' This function allows you to calculate DEG for the IG Isotypes
 #'
-#'
 #' @param Seurat_obj Seurat object with integrated data from VDJ analysis with Isotype assigment 
-#'
 #'
 #' @keywords DEG, single cell, VDJ analysis, B cells, T cells
 #'
@@ -16,6 +14,7 @@
 Isotypes_DEG <- function(Seurat_obj){
   library(Seurat)
   
+  Seurat_obj@meta.data <- Seurat_obj@meta.data[names(Seurat_obj@ident),]
   # replace cell identity from cluster to isotype
   Seurat_obj@ident <- factor(Seurat_obj@meta.data$c_gene_short)
   names(Seurat_obj@ident) <- rownames(Seurat_obj@meta.data)
@@ -54,3 +53,39 @@ Isotypes_DEG <- function(Seurat_obj){
   rimmi.rnaseq::markers_to_xls(Full_iso_markers, filename = 'Full_isotypes_DEGs')
   print('saved full isotypes as an excel spreadsheet')
 }  
+
+#' This function allows you to pull specific barcodes from the object with specific IG Isotypes from specific clusters
+#'
+#'
+#' @param Seurat_obj Seurat object with integrated data from VDJ analysis with Isotype assigment 
+#' @param isotypes Isotype names
+#' @param clusters cluster names
+#'
+#' @keywords VDJ analysis, B cells, T cells
+#'
+#' @examples
+#'
+#' pull_isotype(A07, 'IGHA1')
+#'
+#' @export
+#'
+
+pull_isotype <- function(Seurat_obj, 
+                         isotypes, 
+                         clusters = NULL){
+  
+  Seurat_obj@meta.data <- Seurat_obj@meta.data[names(Seurat_obj@ident),]
+  
+  if (is.null(clusters)){
+  iso.cells <- rownames(
+    Seurat_obj@meta.data)[Seurat_obj@meta.data$c_gene %in% isotypes]
+  } else{
+    iso.cells <- rownames(
+      Seurat_obj@meta.data)[(Seurat_obj@meta.data$c_gene %in% isotypes) & (Seurat_obj@ident %in% clusters)]
+  }
+  iso.cells
+}
+
+
+
+
