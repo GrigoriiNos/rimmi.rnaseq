291c04751395eaad3b67f034449ef45f7f81a8c4
GrigoriiNos <nosgrigoriy@gmail.com>
2020-04-01 14:16:44 +0300

    added geom_split_violin

--- R/Harmony_Seurat.R
+++ R/Harmony_Seurat.R
@@ -64,3 +64,84 @@ harmony_for_seurat <- function(merged_object, pc.genes = 'default'){
 
   merged_object
 }
+
+
+#' use the batch effect correction method scanorama on Seurat objects
+#'
+#' This function allows you to use an additional BE correction method still using Seurat objects
+#'
+#' @param merged_object pre-merged Seurat object
+#'
+#' @return Seurat object with BE corrected expression matrix in the data slot
+#'
+#' @keywords Seurat, single cell sequencing, RNA-seq, gene signature
+#'
+#' @examples
+#'
+#' mnn_for_seurat(B04_B05)
+#' B05@meta.data[, "sample"] <- "B05"
+#' B04@meta.data[, "sample"] <- "B04"
+#'
+#' add prefix to cellnames
+#' B05 <- RenameCells(B05,
+#'                   add.cell.id = 'B05')
+#'
+#' B04 <- RenameCells(B04,
+#'                   add.cell.id = 'B04')
+#'
+#' B04_B05 <- MergeSeurat(B04, B05)
+#'
+#' @export
+#'
+#'
+
+scanorama_seurat <- function(merged_object, raw.data = T){
+  samples <- unique(merged_object@meta.data$orig.ident)
+
+  if (raw.data == F){
+    batch1 <- merged_object@data[, merged_object@meta.data$orig.ident == samples[1]]
+    cells1 <- colnames(batch1)
+
+    batch2 <- merged_object@data[, merged_object@meta.data$orig.ident == samples[2]]
+    cells2 <- colnames(batch2)
+  } else {
+    batch1 <- merged_object@raw.data[, merged_object@meta.data$orig.ident == samples[1]]
+    cells1 <- colnames(batch1)
+
+    batch2 <- merged_object@raw.data[, merged_object@meta.data$orig.ident == samples[2]]
+    cells2 <- colnames(batch2)
+  }
+
+  # List of data sets (matrices of cells-by-genes):
+  datasets <- list(t(as.matrix(batch1)),
+                   t(as.matrix(batch2)))
+  # List of gene lists:
+  genes_list <- list(rownames(batch1),
+                     rownames(batch2))
+
+  library(reticulate)
+  scanorama <- import('scanorama')
+
+  # Batch correction.
+  corrected.data <- scanorama$correct(datasets,
+                                      genes_list,
+                                      return_dense=TRUE)
+
+  batch1 <- corrected.data[[1]][[1]]
+  rownames(batch1) <- cells1
+  colnames(batch1) <- as.character(corrected.data[[2]])
+
+  batch2 <- corrected.data[[1]][[2]]
+  rownames(batch2) <- cells2
+  colnames(batch2) <- as.character(corrected.data[[2]])
+
+  corrected <- t(
+    rbind(batch1,
+          batch2)
+  )
+
+  colnames(corrected) <- c(cells1, cells2)
+
+  merged_object@data <- as(corrected, 'dgCMatrix')
+  merged_object
+}
--- R/volcano_plot.R
+++ R/volcano_plot.R
@@ -34,3 +34,36 @@ volcano_plot <- function(edgeR_output, title = 'volcano plot'){
     abline(h = -1.5, v = 1.5, col = "gray60")
   }
 }
+
+#' plot volcano plot 2
+#'
+#' better version volcano plot
+#'
+#' @param response table with DEG
+#' @param logFC_th LogFC threshold
+#' @param title the name of your plot
+#'
+#' @return volcano plot
+#'
+#' @keywords DEG, differential expression analysis, volcano plot
+#'
+#' @examples
+#'
+#' volcano_pl(responce)
+#'
+#' @export
+
+volcano_pl <- function(response, logFC_th = 0.5, title){
+  table <- response %>%
+    mutate(`-log10p` = -log(adj.P.Val, 10)) %>%
+    mutate(col = ifelse((adj.P.Val < 0.05 & abs(logFC) > 0.5), 'red', 'grey'))
+
+  ggplot(table, aes(x = logFC, y = `-log10p`, color = col)) +
+    geom_point() +
+    scale_color_manual(breaks = c("8", "6"), values=c("blue", "red")) +
+    geom_label_repel(aes(label = gene),
+                     box.padding   = 0.35,
+                     point.padding = 0.5,
+                     segment.color = 'grey50') +
+    ggtitle(title)
+}
